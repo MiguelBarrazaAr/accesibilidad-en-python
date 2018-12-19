f168a3f11cb63321461971be620cc91bff7ecdc1
Miguel Barraza <miguelbarraza2015@gmail.com>
2018-12-19 00:00:35 -0300

    ejemplos con tolk

---
+++ ejemplos con tolk/demo.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import Tolk
+Tolk.load()
+
+def leer(texto, interrumpir=True):
+    Tolk.output(texto, interrumpir)
+
+print("en consola")
+
+leer("este mensaje no se leera porque sera interrumpido")
+leer("primer mensaje leído será este.", True)
+leer("este es un mensaje a continuación", False)
